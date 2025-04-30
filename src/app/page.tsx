59d6a9f1f314a652d8d274541578aca6b2bdecd5
Chanho Lee <tom.dev.kr@gmail.com>
2025-04-30 10:07:43 +0900

    settings: 프로젝트 명 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default async function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>TODO 앱</h1>
+      <h1 className={styles.title}>StrawBoard</h1>
       <Link className={styles.gotoUsers} href="/users">
         jsonplaceholder Users 페이지 이동
       </Link>
